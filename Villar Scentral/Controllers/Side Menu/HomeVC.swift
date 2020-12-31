f0f2777f975f1c69390f7084cacb10a0f0ccdfbe
Happy Guleria <happyguleria123@gmail.com>
2020-12-31 14:09:19 +0530

    updated code

--- Villar Scentral/Controllers/Side Menu/HomeVC.swift
+++ Villar Scentral/Controllers/Side Menu/HomeVC.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 import LGSideMenuController
+import MessageUI
 
 
-class HomeVC: UIViewController {
 
+class HomeVC: UIViewController ,MFMailComposeViewControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func requetRefillButtonAction(_ sender: Any) {
@@ -21,16 +23,32 @@ class HomeVC: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     @IBAction func aboutUsButtonAction(_ sender: Any) {
-         if let url = URL(string: "mailto://azimov@demo.com") {
-             if UIApplication.shared.canOpenURL(url) {
-                 UIApplication.shared.openURL(url)
-             } else {
-                 print("Cannot open URL")
-             }
-         }
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+        
     }
     
-    @IBAction func openMenu(_ sender: Any) {
-        sideMenuController?.showLeftViewAnimated()
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["someone@somewhere.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        alert(Constant.shared.appTitle, message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", view: self)
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
